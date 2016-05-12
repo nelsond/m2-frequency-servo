a60a7a8b2ff6dc478ef50e019eae67894a21d2c9
Nelson Darkwah Oppong <n@darkwahoppong.com>
2016-05-11 21:45:55 -0600

    Fix a few bugs in pid controller
    - wrong initial parameters
    - undo change of filter value (`a_2`)
    - accept `0` as current value in `PID.tick`
    - fix filter calculation (see https://en.wikipedia.org/wiki/Digital_biquad_filter > Direct form 2)

--- lib/pid.py
+++ lib/pid.py
@@ -2,7 +2,7 @@ from collections import deque
 
 class PID:
     def __init__(self, current_value=0., setpoint=0., sampling_interval=1., overall_gain=1., prop_gain=1., int_gain=1., diff_gain=0., min_max=(float('-inf'),float('inf')), offset=0):
-        self.rbuffer = deque([0., 0., 0.], maxlen=3)
+        self.rbuffer = deque([0., 0.], maxlen=2)
 
         self.current_value = current_value
         self.setpoint      = setpoint
@@ -18,7 +18,10 @@ class PID:
 
         self.error = 0.
 
-        self.set_params(sampling_interval, prop_gain, int_gain, diff_gain)
+        self.set_params(sampling_interval=sampling_interval,
+                        prop_gain=prop_gain,
+                        int_gain=int_gain,
+                        diff_gain=diff_gain)
 
     def set_params(self, sampling_interval=False, overall_gain=False, prop_gain=False, int_gain=False, diff_gain=False):
         self.sampling_interval = sampling_interval or self.sampling_interval
@@ -41,25 +44,28 @@ class PID:
             'b_1': i*T - d*4./T,
             'b_2': i*T/2. - p + d*2./T,
             'a_1': 0.,
-            'a_2': 1.
+            'a_2': -1.
         }
 
-    def tick(self, current_value=False):
-        if (current_value): self.current_value = current_value
+    def tick(self, current_value=None):
+        if current_value != None:
+            self.current_value = current_value
+
         self.error = self.current_value - self.setpoint
 
+        a_1 = self.filter_coefficients['a_1']
+        a_2 = self.filter_coefficients['a_2']
         b_0 = self.filter_coefficients['b_0']
         b_1 = self.filter_coefficients['b_1']
         b_2 = self.filter_coefficients['b_2']
-        a_1 = self.filter_coefficients['a_1']
-        a_2 = self.filter_coefficients['a_2']
 
         w = self.rbuffer
         x = self.error
 
         # bilinear transform of continous PID transfer function
-        w.append(x - a_1*w[-1] - a_2*w[-2])
-        y  = b_0*w[-1] + b_1*w[-2] + b_2*w[-3]
+        v = x - a_1*w[-1] - a_2*w[-2]
+        y = b_0*v + b_1*w[-1] + b_2*w[-2]
+        w.append(v)
 
         # offset
         y += self.offset
